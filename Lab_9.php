4239ad0a9a7f0d8abfdde1683022911b0f5e18a1
Boldbaatar270 <b21993088@gmail.com>
2025-12-10 10:52:17 +0800

    Add Lab_9.php with basic HTML structure

---
+++ Lab_9.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="mn">
+<head>
+  <meta charset="utf-8">
+  <title>XAMPP</title>
+</head>
+<body>
+  <h3>XAMPP.png</h3>
+  <img src="xampp.png" alt="xampp" style="max-width:100%;height:auto;">
+</body>
+</html>
